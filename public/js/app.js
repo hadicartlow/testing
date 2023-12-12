444d545b4e05f63c0ef1516dfe2fd8df6852529d
hadicartlow <iqtidar.hadi@cartlow.com>
2023-12-12 11:57:46 +0500

    this

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\xampp\htdocs\testing\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\xampp\htdocs\testing\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /var/www/html/testing/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/testing/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
